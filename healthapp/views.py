397f395bf298ffe584c28fc639b477ecc04c7f56
Samtfm <sfmemail@gmail.com>
2017-09-15 12:38:25 -0700

    add basic index view

--- healthapp/views.py
+++ healthapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Heya, you've made it to the Health App Index!")
