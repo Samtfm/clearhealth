dd3fc4069fca05bfbab0cfc6a905957657430a19
Samtfm <sfmemail@gmail.com>
2017-09-20 15:46:33 -0700

    proper oath route

--- healthapp/views.py
+++ healthapp/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
-
+import urllib
 # Create your views here.
 def index(request):
     return HttpResponse("Heya, you've made it to the Health App Index!")
 
 def auth(request):
-    response = redirect('https://onpatient.com/authorize')
+    # redirect_uri = "https://clear-health.herokuapp.com/"
+    redirect_uri = 'http://localhost:8000/app/'
+    client_id = "ykmll9feaxeRc6g04N8Ysv67z7UKAjrUQ1KwvU87"
+    querystring = urllib.parse.urlencode({
+        "redirect_uri": redirect_uri,
+        "response_type": "code",
+        "client_id": client_id,
+        # "scope": scopes
+    });
+    # response = HttpResponseRedirect(redirect_to=url)
+    response = redirect("https://onpatient.com/o/authorize/?" + querystring)
+    # response = redirect('https://onpatient.com/authorize')
     # Set 'Test' header and then delete
     response['redirect_uri'] = 'https://yahoo.com'
     return response
